9084474239fc7fd8bc395ad8ddf55edf291ae7c8
keisuke <rememberthatk@icloud.com>
2020-04-19 09:55:55 +0900

    [master] add seed

---
+++ database/seeds/BooksTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Book;
+use Illuminate\Database\Seeder;
+
+class BooksTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('books')->truncate();
+        $books = [
+            ['title' => 'PHP Book',
+            'description' => 'this is your book',
+            'price' => 2000,
+            'page_count' => 1000],
+            ['title' => 'Laravel Book',
+            'description' => 'this is your book',
+            'price' => 3000],
+            ['title' => 'Ruby Book',
+            'description' => 'this is your book',
+            'price' => 2500,
+            'page_count' => 100]
+        ];
+
+        foreach($books as $book) {
+            Book::create($book);
+        }
+    }
+}
