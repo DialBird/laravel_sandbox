7bde4d72b22163536b8f71389bb93780a64688fc
keisuke <rememberthatk@icloud.com>
2020-04-28 08:08:28 +0900

    [master] add scope method to Book

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     protected $guarded = ['id'];
+
+    public function scopeSearchByTitle($query, $title)
+    {
+        return $query->where('title', $title);
+    }
 }
