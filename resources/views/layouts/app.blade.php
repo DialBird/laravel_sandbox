faaaa647802c80ecadc2468d51e0c7a143620bab
keisuke <rememberthatk@icloud.com>
2020-04-27 15:47:16 +0900

    [master] add bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     <!-- Styles -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <style>
       html, body {
         background-color: #fff;
